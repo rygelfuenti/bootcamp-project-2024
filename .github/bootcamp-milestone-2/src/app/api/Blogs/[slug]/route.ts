bca3483766183ce54145aa6f1886bf7c85f5f1c3
Rygel Fuentiblanca <fuentiblancarygel@gmail.com>
2024-12-22 17:25:05 -0800

    feat: added post request for blog comments

--- .github/bootcamp-milestone-2/src/app/api/Blogs/[slug]/route.ts
+++ .github/bootcamp-milestone-2/src/app/api/Blogs/[slug]/route.ts
@@ -22,7 +22,51 @@ export async function GET(req: NextRequest, { params }: IParams) {
 	    }
 }
 
-export async function POST(req: NextRequest) {
-	const body = req.body
-	const BlogSlug = 
-}
+export async function POST(req: NextRequest, { params }: IParams) {
+	
+	try {
+		await connectDB();
+			const { slug } =  params;
+			const body = await req.json();
+
+			if (!slug) {
+				return NextResponse.json({ error: 'Slug is required'}, { status: 400});
+			}
+			
+			if (!body || !body.user  || !body.comment) {
+				return NextResponse.json ({error: 'User and comment fields are required' },
+					{ status: 400 }
+				)
+			}
+			const result = await blogSchema.collection.updateOne (
+				{ slug },
+				{
+					$addToSet: {
+						comments: {
+							user: body.user,
+							comment: body.comment,
+							date: body.date,
+						},
+					},
+				}
+			);
+
+			if (result.matchedCount === 0) {
+				return NextResponse.json(
+					{ error: 'Blog Post Not Found'},
+					{ status: 404 }
+				);
+			}
+
+			return NextResponse.json(
+				{ message: 'Comment Added!' },
+				{ status: 200 }
+			);
+			} catch (error) {
+				console.error('Error adding comment:', error);
+				return NextResponse.json(
+					{ error: 'Failed to add comment' },
+					{ status: 500 }
+				);
+			}
+		}
