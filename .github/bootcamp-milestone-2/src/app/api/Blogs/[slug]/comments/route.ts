b726b5454136369cf14184003dcd052f4a6f6aad
Rygel Fuentiblanca <fuentiblancarygel@gmail.com>
2024-12-24 10:27:35 -0800

    fix: fixed post function

--- .github/bootcamp-milestone-2/src/app/api/Blogs/[slug]/comments/route.ts
+++ .github/bootcamp-milestone-2/src/app/api/Blogs/[slug]/comments/route.ts
@@ -1,56 +1,41 @@
 import { NextRequest, NextResponse } from 'next/server';
 import connectDB from '@/database/db';
 import BlogModel from '@/database/blogSchema';
+import { IComment } from '@/database/blogSchema';
 
-interface IParams {
-  params: {
-    slug: string;
-  };
-}
-
-export async function POST(req: NextRequest, { params }: IParams) {
-  try {
-    await connectDB();
-    const { slug } = params;
-    const body = await req.json();
-
-    if (!slug) {
-      return NextResponse.json({ error: 'Slug is required' }, { status: 400 });
+export async function POST(req: NextRequest) {
+  await connectDB();
+  
+  try { 
+    const slug = req.nextUrl.pathname.split('/')[3];
+    const {user, comment } = await req.json();
+    
+    //Check for Valid Input
+    if (!user || !comment) {
+      return NextResponse.json({ error: "User and Comment are required" }, { status: 400 });
     }
 
-    if (!body || !body.user || !body.comment) {
-      return NextResponse.json(
-        { error: 'User and comment fields are required' },
-        { status: 400 }
-      );
+    //Find Blog By the Slug
+    const blog = await BlogModel.findOne({ slug });
+
+    //Check For if Blog is Found
+    if (!blog) {
+      return NextResponse.json({ error: "Blog not Found" }, { status: 404 });
     }
 
-    const result = await BlogModel.collection.updateOne(
-      { slug },
-      {
-        $addToSet: {
-          comments: {
-            user: body.user,
-            comment: body.comment,
-            date: new Date(),
-          },
-        },
-      }
-    );
+    const newComment: IComment = {
+      user,
+      comment,
+      time: new Date(),
+    };
+    
+    blog.comments.push(newComment);
 
-    if (result.matchedCount === 0) {
-      return NextResponse.json(
-        { error: 'Blog Post Not Found' },
-        { status: 404 }
-      );
-    }
-
-    return NextResponse.json({ message: 'Comment Added!' }, { status: 200 });
-  } catch (error) {
-    console.error('Error adding comment:', error);
-    return NextResponse.json(
-      { error: 'Failed to add comment' },
-      { status: 500 }
-    );
+    await blog.save();
+    
+    return NextResponse.json({ comments: blog.comments }, { status: 200});
+  } catch (err) {
+    console.error("Error saving comment:", err);
+    return NextResponse.json({ error: "Comment unsaved" }, { status: 500 });
   }
 }
