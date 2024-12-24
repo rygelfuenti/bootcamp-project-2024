13f7bcdea2e3864bbc2cc4cbd64dcea2d95eb410
Rygel Fuentiblanca <fuentiblancarygel@gmail.com>
2024-12-24 11:07:04 -0800

    .

--- .github/bootcamp-milestone-2/src/app/api/Blogs/[slug]/route.ts
+++ .github/bootcamp-milestone-2/src/app/api/Blogs/[slug]/route.ts
@@ -1,17 +1,23 @@
-import { NextRequest, NextResponse } from 'next/server';
-import connectDB from '@/database/db';
-import BlogModel from '@/database/blogSchema';
 
+import { NextRequest, NextResponse } from 'next/server'
+import connectDB from "@/database/db"
+import blogSchema from "@/database/blogSchema"
 
-export async function GET(req: NextRequest) {
-  await connectDB(); // function from db.ts before
-  const  slug = req.nextUrl.pathname.split('/').pop();
-
-  try {
-    const blog = await BlogModel.findOne({ slug }).orFail();
-    return NextResponse.json(blog);
-  } catch {
-    return NextResponse.json('Blog not found.', { status: 404 });
-  }
+type IParams = {
+		params: {
+			slug: string
+		}
 }
 
+// If { params } looks confusing, check the note below this code block
+export async function GET(req: NextRequest, { params }: IParams) {
+    await connectDB() // function from db.ts before
+		const { slug } = params // another destructure
+
+	   try {
+	        const blog = await blogSchema.findOne({ slug }).orFail()
+	        return NextResponse.json(blog)
+	    } catch (err) {
+	        return NextResponse.json('Blog not found.', { status: 404 })
+	    }
+}
